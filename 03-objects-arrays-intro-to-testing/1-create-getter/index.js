0a6ef1152077e070e1ae2b3dd5444006980705fd
Eugene Mushkov <mushkov_es@enm.spb.ru>
2022-03-22 22:36:14 +0300

    3 module, createGetter task

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,16 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
+ const product = {
+    category: {
+    title: 'Goods'
+    }
+ }
 export function createGetter(path) {
-
+    let [a, b] = path.split('.');
+    return (obj) => {
+        return obj[a][b];
+    }
 }
+const getter = createGetter('category.title')
+console.log(getter(product))
