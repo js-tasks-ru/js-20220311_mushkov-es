cf73cce604755026d52e5a7f2c411b7bdef97f9d
javascriptru <67475495+jsru-1@users.noreply.github.com>
2024-12-26 20:10:16 +0300

    Added solution for task: trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -1,34 +1,26 @@
 /**
  * trimSymbols - removes consecutive identical symbols if they quantity bigger that size
  * @param {string} string - the initial string
- * @param {number} size - the allowed size of consecutive identical symbols
+ * @param {number | undefined} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(str, target = '') {
-    let trimStr = '';
-    let counter = 0;
-    
-    for ( let i = 0; i < str.length; i++) {
-        if (str === undefined) return str;
-        if (target === 0) return '';
-        if (target === '') return str;
-        if (str[i] !== str[i + 1] && counter < target){
-            trimStr += str[i];
-        }
-        if (str[i] !== str[i + 1] && str[i] === str[i - 1]) {
-            counter ++
-        }
-        if (str[i] === str[i + 1] && counter < target){
-            trimStr += str[i];
-            counter++
-        }
-        
-        if (counter >= target && str[i] !== str[i + 1]) {
-            counter = 0;
-            
-        }
-    }
-    return trimStr;
-    
+export function trimSymbols(string, size) {
+  if (size === 0) {
+    return '';
+  }
+
+  if (size === undefined) {
+    return string;
+  }
+
+  const firstSlice = string.slice(0, size);
+  const rest = [...string.slice(size)];
+
+  return rest.reduce((str, char) => {
+    if (!str.endsWith(char.repeat(size))) {
+      str += char;
     }
 
+    return str;
+  }, firstSlice);
+}
