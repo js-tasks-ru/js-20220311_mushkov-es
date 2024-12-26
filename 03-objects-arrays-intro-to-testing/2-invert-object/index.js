ed96746b4f7a2f7aadfb1d0e3ed48188845c1b98
javascriptru <67475495+jsru-1@users.noreply.github.com>
2024-12-26 19:53:42 +0300

    Added solution for task: invertObj

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -1,14 +1,13 @@
 /**
  * invertObj - should swap object keys and values
  * @param {object} obj - the initial object
- * @returns {object | undefined} - returns new object or undefined if nothing did't pass
+ * @returns {object | undefined} - returns the new object or undefined if nothing did't pass
  */
- export function invertObj(obj) {
-    const invertObj = {};
-    if (obj === undefined) return obj;
-    for (const [key, value] of Object.entries(obj)) {
-          invertObj[value] = key;
-    }
-    return invertObj
+export function invertObj(obj) {
+  if (obj) {
+    return Object.entries(obj).reduce((accum, [key, value]) => {
+      accum[value] = key;
+      return accum;
+    }, {});
+  }
 }
-
